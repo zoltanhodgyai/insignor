4bb8cff71ca1f9d86d4e14d764d67ec046887f5d
Zoltan Hodgyai <Zoltan.Hodgyai@msg.group>
2019-02-19 09:50:25 +0200

    Created empty project with Spring.io

---
+++ src/main/java/com/bo/insignor/InsignorApplication.java
@@ -0,0 +1,13 @@
+package com.bo.insignor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsignorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsignorApplication.class, args);
+	}
+
+}
