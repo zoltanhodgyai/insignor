c3d25338e4c09980b52fdd827b45890761accf6d
hodgyaiz <sportkukac14@yahoo.com>
2019-02-19 10:35:58 +0200

    Security disabled Added H2 as database vendor

---
+++ src/main/java/com/bo/insignor/configuration/ApplicationSecurity.java
@@ -0,0 +1,18 @@
+package com.bo.insignor.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/**");
+    }
+}
